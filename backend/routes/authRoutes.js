0653aabd093f2db1582897108be7122cecb15105
Ananya Jannatha <154073445+Patelananya@users.noreply.github.com>
2026-02-03 19:06:35 +0530

    Update authRoutes.js added testing cases

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,24 @@ const router = express.Router();
 const { registerStudent, login, createManager, getAllManagers } = require('../controllers/authController');
 const { authenticate, authorizeAdmin } = require('../middleware/auth');
 
+//testing-start-------------
+const bcrypt = require("bcryptjs");
+const db = require("../config/db");
+
+router.post("/reset-admin", async (req, res) => {
+  const hashed = await bcrypt.hash("admin123", 10);
+
+  db.query(
+    "UPDATE users SET password = ? WHERE email = ?",
+    [hashed, "admin@campuscare.com"],
+    (err) => {
+      if (err) return res.status(500).json(err);
+      res.json({ message: "Admin password reset" });
+    }
+  );
+});
+//testing-end--------------
+
 // Public routes
 router.post('/register/student', registerStudent);
 router.post('/login', login);
